270720a7bf2e9cdc317d411a1fab076bab2032ac
unknown <jreidmke@gmail.com>
2020-11-16 12:49:52 -0600

    completed coin component

--- src/Coin.js
+++ src/Coin.js
@@ -1,17 +1,13 @@
 import {React, useState} from 'react';
 
 const Coin = () => {
-    //set coin state using useState
-    //default coin to 'H' for heads
-    //const [rand, setRand] = useState(0);
-    //const flip = () => setRand(Math.random());
-    //const rand = Math.random();
-    //const heads = rand > .5;
-
-    /* return (
+    const [rand, setRand] = useState(0);
+    const flip = () => setRand(Math.random());
+    const heads = rand > .5;
+    return (
         <div>
-            <h3>{heads ? 'H' : 'T'}<h3>
-            <button>
+            <h3>{heads ? 'H' : 'T'}</h3>
+            <button onClick={flip}>Flip</button>
         </div>
-    )*/
+    )
 }
