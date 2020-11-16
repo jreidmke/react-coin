603fc5c6709d968ae1c5758a4b2ca2d385edbc5f
unknown <jreidmke@gmail.com>
2020-11-16 12:54:04 -0600

    updated coin with new states

--- src/Coin.js
+++ src/Coin.js
@@ -1,13 +1,22 @@
 import {React, useState} from 'react';
 
 const Coin = () => {
+
+    //flip stuff
     const [rand, setRand] = useState(0);
     const flip = () => setRand(Math.random());
     const heads = rand > .5;
+
+    //counter stuff
+    const [count, setCount] = useState(0);
+    const [headCount, setHeadCount] = useState(0);
+    const [tailCount, setTailCount] = useState(0);
+
     return (
         <div>
             <h3>{heads ? 'H' : 'T'}</h3>
             <button onClick={flip}>Flip</button>
+            <h6>Out of {count} flips, you got {headCount} heads and {tailCount} tails. Congratulations, fate is an illusion.</h6>
         </div>
     )
 }
